28a1ad6ba3b705f518b70311111e5942ff947001
Vaclav Haken <vaclav@vaclavkabat.net>
2018-04-24 14:07:04 +0100

    debug no fabric now enabled

--- src/content/WebAPIService/Program.cs
+++ src/content/WebAPIService/Program.cs
@@ -16,28 +16,30 @@ namespace WebAPIService
         {
             try
             {
-//#if DEBUGNOFABRIC
+//-:cnd:noEmit
+#if DEBUGNOFABRIC
 
-//                 var host = WebHost.CreateDefaultBuilder()
-//                    .UseStartup<Startup>()                       
-//                    .Build();
+                 var host = WebHost.CreateDefaultBuilder()
+                    .UseStartup<Startup>()                       
+                    .Build();
 
-//                host.Run();                
+                host.Run();                
 
-//#else
-                // The ServiceManifest.XML file defines one or more service type names.
-                // Registering a service maps a service type name to a .NET type.
-                // When Service Fabric creates an instance of this service type,
-                // an instance of the class is created in this host process.
+#else
+                   // The ServiceManifest.XML file defines one or more service type names.
+                   // Registering a service maps a service type name to a .NET type.
+                   // When Service Fabric creates an instance of this service type,
+                   // an instance of the class is created in this host process.
 
-                ServiceRuntime.RegisterServiceAsync("WebAPIServiceType",
+                   ServiceRuntime.RegisterServiceAsync("WebAPIServiceType",
                     context => new WebAPIService(context)).GetAwaiter().GetResult();
 
                 ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(WebAPIService).Name);
 
                 // Prevents this host process from terminating so services keeps running. 
                 Thread.Sleep(Timeout.Infinite);
-//#endif
+#endif
+//+:cnd:noEmit
             }
             catch (Exception e)
             {

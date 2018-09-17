dd47c82756348798ddeb8e77d7ccce7fab575416
David Guerin <david.guerin@developershelf.com>
2018-09-17 16:58:55 +0100

    final cleanup

--- src/content/WebAPIService/Program.cs
+++ src/content/WebAPIService/Program.cs
@@ -17,35 +17,47 @@ namespace WebAPIService
         {
             try
             {
-//-:cnd:noEmit
-#if DEBUGNOFABRIC
+                if (IsSf)
+                {
+                    // The ServiceManifest.XML file defines one or more service type names.
+                    // Registering a service maps a service type name to a .NET type.
+                    // When Service Fabric creates an instance of this service type,
+                    // an instance of the class is created in this host process.
 
-                 var host = WebHost.CreateDefaultBuilder()
-                    .UseStartup<Startup>()                       
-                    .Build();
+                    ServiceRuntime.RegisterServiceAsync("greenfieldType",
+                        context => new WebApiService(context)).GetAwaiter().GetResult();
 
-                host.Run();                
+                    ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(WebApiService).Name);
 
-#else
-                   // The ServiceManifest.XML file defines one or more service type names.
-                   // Registering a service maps a service type name to a .NET type.
-                   // When Service Fabric creates an instance of this service type,
-                   // an instance of the class is created in this host process.
+                    // Prevents this host process from terminating so services keeps running. 
+                    Thread.Sleep(Timeout.Infinite);
+                }
+                else
+                {
+                    var host = WebHost.CreateDefaultBuilder()
+                        .UseStartup<Startup>()
+                        .Build();
 
-                   ServiceRuntime.RegisterServiceAsync("WebAPIServiceType",
-                    context => new WebApiService(context)).GetAwaiter().GetResult();
-
-                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(WebApiService).Name);
-
-                // Prevents this host process from terminating so services keeps running. 
-                Thread.Sleep(Timeout.Infinite);
-#endif
-//+:cnd:noEmit
+                    host.Run();
+                }
             }
             catch (Exception e)
             {
                 BigBrother.Write(e);
                 throw;
+            }
+        }
+
+        /// <summary>
+        /// Determines if you are running in Service Fabric or not
+        /// </summary>
+        private static bool IsSf
+        {
+            get
+            {
+                var sfAppName = Environment.GetEnvironmentVariable("Fabric_ApplicationName");
+                var isSf = sfAppName != null;
+                return isSf;
             }
         }
     }

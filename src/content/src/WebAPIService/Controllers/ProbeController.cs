aa87f725220cdb06fb1e1434d9fbfb4ad7a1560c
David Guerin <david.guerin@developershelf.com>
2018-09-21 15:29:38 +0100

    restructing solution

---
+++ src/content/src/WebAPIService/Controllers/ProbeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIService.Controllers
+{
+    /// <summary>
+    /// health probe controller
+    /// </summary>
+    [Produces("application/json")]
+    [Route("[controller]")]
+    [AllowAnonymous]
+    public class ProbeController : Controller
+    {
+        /// <summary>
+        /// empty get to serve as health probe endpoint
+        /// </summary>
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+    }
+}
---
+++ src/content/tests/WebAPIService.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Eshopworld.Tests.Core;
+using Xunit;
+
+namespace WebAPIService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact, IsUnit]
+        public void Test1()
+        {
+
+        }
+    }
+}

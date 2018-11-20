d41673899690cf3e038a9313d6e196d396e860be
Ruaidhrí Mc Donnell <ruaidhri.mcdonnell+gh@gmail.com>
2018-11-20 17:53:19 +0000

    include examples of response code mapping/descriptions (#15)
    * include aexamples of response code mapping/descriptions
    
    * Update swagger docs and responses

--- src/content/src/WebAPIService/Controllers/ValuesController.cs
+++ src/content/src/WebAPIService/Controllers/ValuesController.cs
@@ -18,8 +18,10 @@ namespace WebAPIService.Controllers
         /// GET implementation for default route
         /// </summary>
         /// <returns>see response code to response type metadata, list of all values</returns>
+        /// <response code="200">List of all values</response>
+        /// <response code="401">Caller is Unauthorized</response>
         [HttpGet]
-        [ProducesResponseType(typeof(string[]), (int) HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string[]), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
         public async Task<IActionResult> Get()
         {
@@ -31,6 +33,9 @@ namespace WebAPIService.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns>see response code to response type metadata, individual value for a given id</returns>
+        /// <response code="200">individual value for a given id</response>
+        /// <response code="401">Caller is Unauthorized</response>
+        /// <response code="404">No entry exists for the given id</response>
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
@@ -45,16 +50,20 @@ namespace WebAPIService.Controllers
         /// </summary>
         /// <param name="value">payload</param>
         /// <returns>action result</returns>
+        /// <response code="201">A new value was created</response>
+        /// <response code="400">The request is malformed</response>
+        /// <response code="401">Caller is Unauthorized</response>
         [HttpPost]
-        [ProducesResponseType((int) HttpStatusCode.OK)]
-        [ProducesResponseType((int) HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
         public async Task<IActionResult> Post([FromBody]string value)
         {
+
             if (string.IsNullOrWhiteSpace(value))
                 return await Task.FromResult(BadRequest());
             else
-                return await Task.FromResult(Ok());
+                return await Task.FromResult(CreatedAtAction("POST", new { id = value }, value));
         }
 
         /// <summary>
@@ -63,11 +72,15 @@ namespace WebAPIService.Controllers
         /// <param name="id">id to process</param>
         /// <param name="value">payload</param>
         /// <returns>action result</returns>
+        /// <response code="200">The put operation was successful</response>
+        /// <response code="400">The request is malformed</response>
+        /// <response code="401">Caller is Unauthorized</response>
+        /// <response code="404">No entry exists for the given id</response>
         [HttpPut("{id}")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> Put(int id, [FromBody]string value)
         {
             if (string.IsNullOrWhiteSpace(value))
@@ -81,13 +94,16 @@ namespace WebAPIService.Controllers
         /// </summary>
         /// <param name="id">id to delete</param>
         /// <returns>action result</returns>
+        /// <response code="200">The delete operation was successful</response>
+        /// <response code="401">Caller is Unauthorized</response>
+        /// <response code="404">No entry exists for the given id</response>
         [HttpDelete("{id}")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
-        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> Delete(int id)
         {
-            return await Task.FromResult(Ok());
+            return await Task.FromResult(NoContent());
         }
     }
 }
